d12ce22187b74b2596e2cde40203896958cc64d5
Ricky Sandoval <rsandoval@arrow.com>
2019-05-23 17:34:57 -0600

    mispelling

--- src/components/StartScreen.js
+++ src/components/StartScreen.js
@@ -8,7 +8,7 @@ import './StartScreen.css';
 export function StartScreen(props) {
     return (
         <div className="modal">
-            <div className="modal__title">Player 1 - Chose a Color</div>
+            <div className="modal__title">Player 1 - Choose a Color</div>
             <div className="modal__controls">
                 <button className="button__red" onClick={() => props.onChooseColor(PLAYER_OPTIONS[0])}>{PLAYER_OPTIONS[0]}</button>
                 <button className="button__black" onClick={() => props.onChooseColor(PLAYER_OPTIONS[1])}>{PLAYER_OPTIONS[1]}</button>
